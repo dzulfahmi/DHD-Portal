93ed7332cdb938affa830d9cc3562ede34e480f1
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2017-09-12 17:18:09 +0700

    membuat semua jadi text

--- index.android.js
+++ index.android.js
@@ -22,12 +22,15 @@ import {
   Card, 
   CardItem, 
   Drawer,
+  Footer,
+  FooterTab,
+  Icon,
   Fab,
+  Badge,
   List,
   ListItem, 
   Thumbnail, 
-  Button, 
-  Icon, 
+  Button,  
   Left, 
   Body, 
   Right,
@@ -41,15 +44,23 @@ import Tab3 from './tabThree';
 const { height, width } = Dimensions.get('window');
 
 export default class DHD_Portal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      active: 'false'
+    };
+  }
   render() {
-    closeDrawer = () => {
-      this.drawer._root.close()
-    };
-    openDrawer = () => {
-      this.drawer._root.open()
-    };
     return (
       <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Input placeholder="Search" />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
         <Content>
           {/*
           <Tabs style={{backgroundColor:'red'}}>
@@ -64,146 +75,60 @@ export default class DHD_Portal extends Component {
             </Tab>
           </Tabs>
           */}
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={{uri: 'https://dummyimage.com/600x400/000/fff'}} />
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>April 15, 2016</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={{uri: 'https://dummyimage.com/600x400/000/fff'}} style={{height: 200, width: 0.9 * width , flex: 1}}/>
-                <Text>
-                  Berita 1
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-                  <Text> 1,926 stars</Text>
-                </Button>
-              </Left>
-            </CardItem>
+          <Card>
+          <CardItem>
+            <Body>
+              <Text>
+                Berita 1
+              </Text>
+              <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </Text>
+            </Body>
+          </CardItem>
           </Card>
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={{uri: 'https://dummyimage.com/600x400/000/fff'}} />
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>April 15, 2016</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={{uri: 'https://dummyimage.com/600x400/000/fff'}} style={{height: 200, width: 0.9 * width , flex: 1}}/>
-                <Text>
-                  Berita 1
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-                  <Text> 1,926 stars</Text>
-                </Button>
-              </Left>
-            </CardItem>
+          <Card>
+          <CardItem>
+            <Body>
+              <Text>
+                Berita 1
+              </Text>
+              <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </Text>
+            </Body>
+          </CardItem>
           </Card>
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={{uri: 'https://dummyimage.com/600x400/000/fff'}} />
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>April 15, 2016</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={{uri: 'https://dummyimage.com/600x400/000/fff'}} style={{height: 200, width: 0.9 * width , flex: 1}}/>
-                <Text>
-                  Berita 1
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-                  <Text> 1,926 stars</Text>
-                </Button>
-              </Left>
-            </CardItem>
-          </Card>
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={{uri: 'https://dummyimage.com/600x400/000/fff'}} />
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>April 15, 2016</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={{uri: 'https://dummyimage.com/600x400/000/fff'}} style={{height: 200, width: 0.9 * width , flex: 1}}/>
-                <Text>
-                  Berita 1
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-
-                  <Text> 1,926 stars</Text>
-                </Button>
-              </Left>
-            </CardItem>
-          </Card>
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={{uri: 'https://dummyimage.com/600x400/000/fff'}} />
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>April 15, 2016</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={{uri: 'https://dummyimage.com/600x400/000/fff'}} style={{height: 200, width: 0.9 * width , flex: 1}}/>
-                <Text>
-                  Berita 1
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-                  <Text> 1,926 stars</Text>
-                </Button>
-              </Left>
-            </CardItem>
+          <Card>
+          <CardItem>
+            <Body>
+              <Text>
+                Berita 1
+              </Text>
+              <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </Text>
+            </Body>
+          </CardItem>
           </Card>
         </Content>
-        <Header searchBar rounded>
-          <Item>
-            <Input placeholder="Search" />
-          </Item>
-          <Button transparent>
-            <Text>Search</Text>
-          </Button>
-        </Header>
+        <View>
+          <Fab
+            active={this.state.active}
+            direction="up"
+            containerStyle={{ }}
+            style={{ backgroundColor: '#5067FF' }}
+            position="bottomRight"
+            onPress={() => this.setState({ active: !this.state.active })}>
+            <Icon name='A'/>
+            <Button style={{ backgroundColor: '#34A34F' }}>
+              <Icon name='A1'/>
+            </Button>
+            <Button style={{ backgroundColor: '#3B5998' }}>
+              <Icon name='A2'/>
+            </Button>
+            <Button disabled style={{ backgroundColor: '#DD5144' }}>
+              <Icon name='A3'/>
+            </Button>
+          </Fab>
+        </View>
       </Container>
     );
   }
